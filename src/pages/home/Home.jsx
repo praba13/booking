e1c1f6aafe36e37a4718bdcd9d44ae35de8f2b52
Praba <prama1303@gmail.com>
2022-04-26 13:54:42 +0200

    Navbar finished

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,8 @@
+import './Home.css';
+import Navbar from '../../components/navbar/Navbar';
+
+const Home = () => {
+  return <div>{<Navbar />}</div>;
+};
+
+export default Home;
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const List = () => {
+  return <div>Lists</div>;
+};
+
+export default List;
