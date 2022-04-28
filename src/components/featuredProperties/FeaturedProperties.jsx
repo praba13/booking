353d5ada17defc3cb672a212f07ad485b1ea002a
Praba <prama1303@gmail.com>
2022-04-28 13:04:07 +0200

    Exp. Image of Hotels finished

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import './FeaturedProperties.css';
 
 const FeaturedProperties = () => {
   return <div>FeaturedProperties</div>;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import './Home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
+import PropertyList from '../../components/propertyList/PropertyList';
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
       <Header />
       <div className='homeContainer'>
         <Featured />
+        <h1 className='homeTitle'>Browse by Property Type</h1>
+        <PropertyList />
       </div>
     </div>
   );
