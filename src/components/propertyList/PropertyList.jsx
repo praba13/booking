0c437d0392c9c739ff17cd4f43211890476f3bf3
Praba <prama1303@gmail.com>
2022-04-28 13:49:36 +0200

    Img of Rooms and Properties Hotel finished

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -10,11 +10,10 @@ const PropertyList = () => {
           className='pListImg'
         />
         <div className='pListTitles'>
-          <h1>Hotes</h1>
-          <h2>123 hotel</h2>
+          <h1>Hotels</h1>
+          <h2>233 hotels</h2>
         </div>
       </div>
-
       <div className='pListItem'>
         <img
           src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg'
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
 import Featured from '../../components/featured/Featured';
 import PropertyList from '../../components/propertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 const Home = () => {
   return (
@@ -13,6 +14,8 @@ const Home = () => {
         <Featured />
         <h1 className='homeTitle'>Browse by Property Type</h1>
         <PropertyList />
+        <h1 className='homeTitle'>Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
