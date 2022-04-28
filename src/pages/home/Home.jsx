4c8cd79fab4399c3c10f20ff59ae6b72d7254f45
Praba <prama1303@gmail.com>
2022-04-28 12:21:19 +0200

    Exp. Image of place is finished

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import './Home.css';
 import Navbar from '../../components/navbar/Navbar';
 import Header from '../../components/header/Header';
+import Featured from '../../components/featured/Featured';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className='homeContainer'>
+        <Featured />
+      </div>
     </div>
   );
 };
