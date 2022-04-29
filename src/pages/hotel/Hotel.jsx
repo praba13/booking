1645267138aa08f7bf9f7761751c13471ae766f0
Praba <prama1303@gmail.com>
2022-04-30 00:13:35 +0200

    Hotel Img with Slider finished

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Hotel.css';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
@@ -13,6 +14,8 @@ import MailList from '../../components/mailList/MailList';
 import Footer from '../../components/footer/Footer';
 
 const Hotel = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [open, setOpen] = useState(false);
   const photos = [
     {
       src: 'https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1'
@@ -34,14 +37,53 @@ const Hotel = () => {
     }
   ];
 
+   const handleOpen = (i) => {
+     setSlideNumber(i);
+     setOpen(true);
+   };
+
+   const handleMove = (direction) => {
+     let newSlideNumber;
+
+     if (direction === 'l') {
+       newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
+     } else {
+       newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
+     }
+
+     setSlideNumber(newSlideNumber);
+   };
+
   return (
     <div>
       <Navbar />
       <Header type='list' />
       <div className='hotelContainer'>
+        {open && (
+          <div className='slider'>
+            <FontAwesomeIcon
+              icon={faCircleXmark}
+              className='close'
+              onClick={() => setOpen(false)}
+            />
+            <FontAwesomeIcon
+              icon={faCircleArrowLeft}
+              className='arrow'
+              onClick={() => handleMove('l')}
+            />
+            <div className='sliderWrapper'>
+              <img src={photos[slideNumber].src} alt='' className='sliderImg' />
+            </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              className='arrow'
+              onClick={() => handleMove('r')}
+            />
+          </div>
+        )}
         <div className='hotelWrapper'>
-          <button className='bookNow'>Reserve Or BookNow</button>
-          <h1 className='hotelTitle'>Grand Hotel</h1>
+          <button className='bookNow'>Reserve or Book Now!</button>
+          <h1 className='hotelTitle'>Tower Street Apartments</h1>
           <div className='hotelAddress'>
             <FontAwesomeIcon icon={faLocationDot} />
             <span>Elton St 125 New york</span>
@@ -52,14 +94,19 @@ const Hotel = () => {
           <span className='hotelPriceHighlight'>
             Book a stay over $114 at this property and get a free airport taxi
           </span>
-          <div className='hotelImgages'>
-            {photos.map((photo) => (
-              <div className='hotelImgWrapper'>
-                <img src={photo.src} alt='' className='hotelImg' />
+          <div className='hotelImages'>
+            {photos.map((photo, i) => (
+              <div className='hotelImgWrapper' key={i}>
+                <img
+                  onClick={() => handleOpen(i)}
+                  src={photo.src}
+                  alt=''
+                  className='hotelImg'
+                />
               </div>
             ))}
           </div>
-          <div className='hotelDetails '>
+          <div className='hotelDetails'>
             <div className='hotelDetailsTexts'>
               <h1 className='hotelTitle'>Stay in the heart of City</h1>
               <p className='hotelDesc'>
@@ -76,7 +123,7 @@ const Hotel = () => {
                 airport shuttle service.
               </p>
             </div>
-            <div className='hotelDetailsPrice '>
+            <div className='hotelDetailsPrice'>
               <h1>Perfect for a 9-night stay!</h1>
               <span>
                 Located in the real heart of Krakow, this property has an
